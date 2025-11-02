43d22fb26a066c62a19c69df439dd0c06d0d95c3
Kitahara7023 <human.kagoshima.naozumi@gmail.com>
2025-11-02 23:02:25 +0900

    [add]Ball

--- Assets/Scenes/Ball.cs
+++ Assets/Scenes/Ball.cs
@@ -4,24 +4,49 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject ballPrefab;
+    //[SerializeField] private GameObject ballPrefab;
+    [SerializeField] private float speed = 5f;
+    private Rigidbody2D myRigidbody;
 
-    public void Initialize() { }
-
-    void Start()
+    void Awake()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
+    public void Initialize(Vector2 startPosition)
+    {
+        transform.position = startPosition; // バーの上に配置
+
+        if (myRigidbody != null)
+        {
+            // 上方向に飛ばす（ブロック崩しらしい動き）
+            myRigidbody.linearVelocity = Vector2.up * speed;
+        }
+    }
+}
+    /*
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
             var newball = Instantiate(ballPrefab);
+
             var ball = newball.GetComponent<Ball>();
+
             ball.Initialize();
-        }    
+        }
     }
+    public void Initialize() 
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        if (myRigidbody != null)
+        {
+            
+            myRigidbody.linearVelocity = new Vector2(1, 1).normalized * speed;
+        }
+    }
+
+   
 }
+    */
