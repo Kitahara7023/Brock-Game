38689c994fb588f03fb0042a4c7af4d553a63243
Kitahara7023 <human.kagoshima.naozumi@gmail.com>
2025-12-08 13:18:13 +0900

    Block

--- Assets/Scenes/Block.cs
+++ Assets/Scenes/Block.cs
@@ -4,6 +4,57 @@ using System.Collections.Generic;
 
 public class Block : MonoBehaviour
 {
+    private SpriteRenderer sr;
+
+
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        Color c = sr.color;
+        c.a = 255f;
+        sr.color = c;
+    }
+
+    public void Initialize()
+    {
+        StartCoroutine(FadeIn(1.0f));
+    }
+
+    public void OnStart()
+    {
+
+    }
+
+    public void OnUpdate()
+    {
+
+    }
+
+    private IEnumerator FadeIn(float duration)
+    {
+        float elapsed = 0f;
+
+        Color c = sr.color;
+        float startA = 0f;
+        float endA = 1f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+
+            c.a = Mathf.Lerp(startA, endA, t);
+            sr.color = c;
+
+            yield return null;
+        }
+
+        c.a = 1f;
+        sr.color = c;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ball")
