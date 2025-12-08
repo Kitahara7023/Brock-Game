c2eb3e2356498a870f0560c2842d5e3f4aff4338
Kitahara7023 <human.kagoshima.naozumi@gmail.com>
2025-12-08 13:19:19 +0900

    Stage[追加]

--- Assets/Scenes/Stage.cs
+++ Assets/Scenes/Stage.cs
@@ -1,12 +1,15 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
     [SerializeField] GameObject Ballprehub;
-    
-    private Ball _ball;
-    
+    [SerializeField] GameObject Blockprehub;
 
+    private Ball _ball;
+    private Block _block;
+    private List<Block> _blocks = new List<Block>();
 
     void Start()
     {
@@ -14,11 +17,32 @@ public class Stage : MonoBehaviour
         var ball = ballObject.GetComponent<Ball>();
         ball.OnStart();
         _ball = ball;
+
+        const int blockCount = 15;
+        const int columnMax = 5;
+        const float blockWidth = 0.5f;
+        const float blockHeight = 0.25f;
+
+        var offsetX = (columnMax / 2f * blockWidth) - blockWidth / 2;
+
+        for (int i = 0; i < blockCount; i++)
+        {
+            var blockObject = Instantiate(Blockprehub);
+            var block = blockObject.GetComponent<Block>();
+            _blocks.Add(block);
+
+            block.OnStart();
+            var x = i % columnMax * blockWidth;
+            var y = i / columnMax * blockHeight;
+            block.gameObject.transform.localPosition = new Vector2(x - offsetX, y);
+        }
+
+        Debug.Log("_block.Count"+ _blocks.Count);
+
     }
-   
+
     void Update()
     {
         _ball.OnUpdate();
-        
     }
 }
